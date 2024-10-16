04eb82d180d8714986dad97384d5f5a06f2781e4
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-16 15:14:55 +0200

    snack 4

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -5,5 +5,10 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+authors=posts.map((post)=>{
+  return post.author
+})
+console.log(authors);
+
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
